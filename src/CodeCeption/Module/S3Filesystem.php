fc5c88e4c6cda496e70397dd1625946b0f9e8134
Iain <iain@polevaultweb.com>
2016-01-27 12:15:22 +0000

    Initial commit

---
+++ src/CodeCeption/Module/S3Filesystem.php
@@ -0,0 +1,45 @@
+<?php
+namespace Codeception\Module;
+
+use Aws\S3\S3Client;
+
+class S3Filesystem extends Filesystem {
+
+	/**
+	 * @var array
+	 */
+	protected $requiredFields = array( 'accessKey', 'accessSecret' );
+
+	/**
+	 * @va
+	 */
+	protected $client;
+
+	/**
+	 * Initialize the S3 client
+	 */
+	public function _initialize() {
+		parent::_initialize();
+
+		$args = array(
+			'version'     => isset( $this->config['version'] ) ? $this->config['version'] : 'latest',
+			'region'      => isset( $this->config['region'] ) ? $this->config['region'] : 'us-east-1',
+			'signature'   => isset( $this->config['signature'] ) ? $this->config['signature'] : 'v4',
+			'credentials' => array(
+				'key'    => $this->config['accessKey'],
+				'secret' => $this->config['accessSecret'],
+			),
+		);
+
+		$this->client = new S3Client( $args );
+	}
+
+	/**
+	 * Delete a Bucket
+	 *
+	 * @param string $bucket
+	 */
+	public function deleteBucket( $bucket ) {
+		$this->client->deleteBucket( array( 'Bucket' => $bucket ) );
+	}
+}
